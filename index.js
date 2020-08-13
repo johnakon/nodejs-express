c11ff2bfb0bbcbf700810d9995b8005784a4fc8b
johnakon <john.akonya@gmail.com>
2020-08-13 13:46:02 +0300

    modified the index.js file to include session middleware

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
+const routes = require('./routes/route');
+
+/* require service classes */
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+/* now creating instances of the classes */
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
 
 const app = express();
 
@@ -7,13 +18,26 @@ app.use(express.static(path.join(__dirname, '/static')));
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/index.html'));
-});
+app.set('trust proxy', 1);
 
-app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['GHTu75TR4', 'dfTqwEr9u4'],
+  })
+);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakerService,
+    /* speakerService:speakerService */
+  })
+);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
